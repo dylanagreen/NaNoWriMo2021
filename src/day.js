76368ee56b1a076bbf1b8fb57185ead0d16ef0db
dylanagreen <dylan@mygreenfamily.us>
2021-11-02 17:14:40 -0700

    Rework website a bit to put Day 1 onto its own page.

--- src/day.js
+++ src/day.js
@@ -1,9 +1,14 @@
 var temp_data = "";
 
+// Get the "dayX" part of the url.
+// A little messy but it works tm?
+let day_num = document.URL.split("/").pop().split(".html")[0]
+
+
 // Using AJAX to get the text from the day.
 $.ajax({
   async:false,
-  url: 'days/day_1.txt',
+  url: "../days_txt/" + day_num + ".txt",
   dataType: 'text',
   success: function(data)
   {
@@ -13,6 +18,3 @@ $.ajax({
 
 temp_data = temp_data.replace(/(?:\r\n|\r|\n)/g, "<br>&emsp;");
 document.write(temp_data);
-
-console.log(document.URL)
-console.log(window.location.href)
