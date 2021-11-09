b104a69b186e55fd9b367bbf0c35b3dd5c091a11
dylanagreen <dylan@mygreenfamily.us>
2021-11-08 16:15:25 -0800

    Add the story so far, a page for the entire story at once.

---
+++ src/sofar.js
@@ -0,0 +1,21 @@
+
+let colors = ["#000080", "#000000"];
+
+for (let i = 1; i < 31; i++) {
+    // Using AJAX to get the text from the day.
+  $.ajax({
+    async:false,
+    url: "../days_txt/day" + String(i) + ".txt",
+    dataType: 'text',
+    success: function(data)
+    {
+      let temp_data = data.replace(/(?:\r\n|\r|\n)/g, "<br>&emsp;");
+      p = document.createElement("p");
+      p.id = "day" + String(i);
+      p.style.color = colors[i % colors.length];
+      p.innerHTML += temp_data;
+      p = document.body.appendChild(p);
+    }
+      });
+
+}
